34c281a180ad58ba46e2ba643c3cfea277e42a94
Bren Briggs <bbriggs@users.noreply.github.com>
2017-07-19 08:09:17 -0400

    Add some comments

--- flaghammer.go
+++ flaghammer.go
@@ -12,12 +12,16 @@ func main() {
 	flagFile := args[0]
 	flagString := []byte(args[1])
 	for {
+		// this "test then read" pattern has a race condition
 		if _, err := os.Stat(flagFile); !os.IsNotExist(err) {
+			// Deleting the file between os.Stat and ioutil.ReadFile
+			// will produce an error that causes the program to exit
 			data, err := ioutil.ReadFile(flagFile)
 			if err != nil {
 				fmt.Println("%s\n", err)
 				os.Exit(1)
 			}
+			// Only write when file exists and doesn't match flagString
 			if !bytes.Equal(data, flagString) {
 				ioutil.WriteFile(flagFile, flagString, 0644)
 			}
